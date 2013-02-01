02182adf3002609f330c7009abecf59003a16558
mdemo <mengdesen09@qq.com>
2013-02-01 14:44:39 +0800

    增加写入数据模块

--- sdk/js/endclient.js
+++ sdk/js/endclient.js
@@ -4,33 +4,97 @@
  * Time: 下午3:32
  * Desc: End.js javascript sdk
  */
-(function (win) {
-    /**
-     * end框架的全局对象，也是框架内部所有类的命名空间
-     */
-    var end = win.end = {
-        version:"0.0",
-        global:win
-    };
+(function () {
     var _vars = {
         socket:{}
     };
-    function Base(route){
-        this.route = route;
-    }
-    Base.prototype.child = function(route){
-        return new Base(this.route+'.'+route);
+    var root = window;
+    /**
+     * End.js 数据构造器
+     * @param route 数据路由地址 可为空，默认为data
+     * @param url   后端数据服务地址 可为空 如果存在，将对webSocket进行初始化
+     * @constructor
+     */
+    var End = function (route, url) {
+        this.route = route || '';
+        if (url) {
+            _vars.socket = io.connect(url);
+            _vars.socket.on('news', function (data) {
+                _vars.socket.emit('my other event', {my:'data'},function(success){
+                    console.log(success);
+                });
+            });
+        }
     };
-    Base.prototype.parent = function(){
-        return new Base();
+    root.End = End;
+    /**
+     * 根据路由，获取子元素
+     * @param route 不可为空
+     * @return 子元素实体
+     */
+    End.prototype.child = function (route) {
+        return new End(this.route + '.' + route);
     };
-    end.init = function(url,app){
-       _vars.socket = io.connect(url);
-       _vars.socket.on('news',function(data){
-          console.log(data);
-          _vars.socket.emit('my other event',{my: 'data'});
-       });
-       return new Base();
+    /**
+     * 获取父元素
+     * @return 父元素实体
+     */
+    End.prototype.parent = function () {
+        var route = this.route.substring(0, this.route.lastIndexOf('.'));
+        return new End(route);
+    };
+    /**
+     * 数据处理通用方法
+     * @param value 要处理的数据
+     * @param method 方法类型
+     * @param callback 回调函数
+     */
+    End.prototype.data = function(value,method,callback){
+        var data = {};
+        data.routes = this.route.split('.');
+        data.value = value;
+        data.method = method;
+        _vars.socket.emit('dal',data,function(returns){
+            if(callback){
+                callback(returns);
+            }
+        });
+    };
+    /**
+     * collections元素添加
+     * @param value 要添加的数据
+     * @param callback 回调函数 成功返回id
+     */
+    End.prototype.insert = function(value,callback){
+        this.data(value,'insert',function(returns){
+            callback(returns);
+        });
+    };
+    /**
+     * 添加或修改非数组数据
+     * @param value 要添加的数据
+     * @param callback 回调函数 会返回是否成功
+     */
+    End.prototype.set = function (value,callback) {
+        this.data(value,'set',function(result){
+            callback(result);
+        });
+    };
+    /**
+     * 添加数组数据
+     * @param value 要添加的数组元素
+     * @param callback 回调函数 会返回是否成功
+     */
+    End.prototype.push = function(value,callback){
+        this.data(value,'push',callback);
+    };
+    End.prototype.update = function (value,callback) {
+        this.data(value,'update',function(result){
+            callback(result);
+        });
+    };
+    End.prototype.on = function () {
+
     };
 
-}(window));
+}());
