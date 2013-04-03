7ee3b559bfae6a37893110fee5cc1086e9859324
mdemo <mengdesen09@qq.com>
2013-04-04 00:03:10 +0800

    add demo

--- demo/chat/js/chat.js
+++ demo/chat/js/chat.js
@@ -2,5 +2,17 @@
  * author: mdemo
  * Date: 13-4-2
  * Time: 下午10:36
- * Desc:
+ * Desc: chatRoom by End.js
  */
+var chat = new End('chat','http://localhost:8080'),
+    chatRoom = chat.child(window.location.search.split('?')[1]),
+    chatMsgs = chatRoom.child('msgs');
+chatMsgs.on('child_added',function(msg){
+    $('.msgs').append('<div class="msg">'+msg.value.name+' : '+msg.value.text+'</div>');
+});
+$('#btn_send').click(function(){
+    var name = $('.txt_send_name').val();
+    var text = $('.txt_send_text').val();
+    chatMsgs.push({name:name,text:text});
+});
+
